9c9fe7ba21eb6a43551ad48cd4fbfac838c316e6
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-12-09 05:44:09 -0500

    now use printf to format number to machine name

--- check-bahen.sh
+++ check-bahen.sh
@@ -8,10 +8,10 @@ cdf_login='g0zee'
 
 for lab in 2210:28 2220:24 2240:12 3175:18 3185:24 3195:18
 do
-  for station in $(eval echo "{01..${lab##*:}}")
+  for station in $(eval echo "{1..${lab##*:}}")
   do
     command="
-    machine=\"b${lab%:*}-${station}\";
+    printf -v machine \"b%s-%#02d\" ${lab%:*} ${station};
     output=\$(ssh -qq ${cdf_login}@b${lab%:*}-${station}.cdf.toronto.edu users);
     ssh_exit_code=\$?;
     if [ \"\${ssh_exit_code}\" -eq \"0\" ];
