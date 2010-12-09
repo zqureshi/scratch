9964701205b10a17fd48c480f1edc8646e01bf6c
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-12-09 04:29:11 -0500

    check exit code of ssh process to display message

--- check-bahen.sh
+++ check-bahen.sh
@@ -10,7 +10,20 @@ for lab in 2210:28 2220:24 2240:12 3175:18 3185:24 3195:18
 do
   for station in $(eval echo "{01..${lab##*:}}")
   do
+    # Echo machine name
     echo -n "b${lab%:*}-${station}: "
-    echo $(ssh ${cdf_login}@b${lab%:*}-${station}.cdf.toronto.edu users)
+
+    # Get logged-in users
+    output=$(ssh -qq ${cdf_login}@b${lab%:*}-${station}.cdf.toronto.edu users)
+    ssh_exit_code=$?
+
+    # If exited successfully, display users
+    if [ "$ssh_exit_code" -eq "0" ]
+    then
+      echo "${output}"
+    else
+      echo "down.."
+    fi
+
   done
 done
