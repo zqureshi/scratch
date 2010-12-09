e555a1075f1ddb648a7646843b1e8f7dd6ebd41a
Zeeshan Qureshi <zeeshan@zqureshi.in>
2010-12-09 04:10:52 -0500

    now store lab info as keypair of room:no_of_stations

--- check-bahen.sh
+++ check-bahen.sh
@@ -6,11 +6,11 @@
 
 cdf_login='g0zee'
 
-for lab in 2210 2220 2240 2270 3175 3185 3195
+for lab in 2210:28 2220:24 2240:12 3175:18 3185:24 3195:18
 do
-  for station in {01..40}
+  for station in $(eval echo "{01..${lab##*:}}")
   do
-    echo -n "b${lab}-${station}: "
-    echo `ssh ${cdf_login}@b${lab}-${station}.cdf.toronto.edu users`
+    echo -n "b${lab%:*}-${station}: "
+    echo $(ssh ${cdf_login}@b${lab%:*}-${station}.cdf.toronto.edu users)
   done
 done
